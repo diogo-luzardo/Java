9a21f368723185d5c597d2416520bb35321bd1f9
diogo-luzardo <54155950+diogo-luzardo@users.noreply.github.com>
2019-11-02 16:55:57 -0300

    Exercicio finalizado

--- exercicioList1/src/application/Program.java
+++ exercicioList1/src/application/Program.java
@@ -26,12 +26,30 @@ public class Program {
 			int id = sc.nextInt();
 			System.out.println("Name: ");
 			String name = sc.nextLine();
-			System.out.println("Salary");
+			System.out.print("Salary");
 			double salary = sc.nextDouble();
 			list.add(new Employee(id, name, salary));
 		}
 		
+		System.out.println();
+		System.out.println("Enter the employee id that will have salary increase: ");
+		int id = sc.nextInt();
+		Employee emp = list.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
+			if (emp == null) {
+				System.out.print("This id does not exist!");
+			}else {
+				System.out.print("Enter the percentage: ");
+				double percentage = sc.nextDouble();
+				emp.increaseSalary(percentage);
+		}
+			
+		System.out.println();
+		System.out.println("List of employees: ");
+		for (Employee obj : list) {
+			System.out.println(obj);
+		}
 		
+		sc.close();
 	}
 
 }
