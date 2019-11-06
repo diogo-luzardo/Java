37b9b2ef40faa9e77f4bc2f6cbda8e2db3b99bf4
diogo-luzardo <54155950+diogo-luzardo@users.noreply.github.com>
2019-11-06 19:06:51 -0300

    Aula de condicionais inicio

---
+++ sintaxe-variaveis-e-fluxo/src/Variaveis/TestaCondicional.java
@@ -0,0 +1,23 @@
+package Variaveis;
+
+public class TestaCondicional {
+	 public static void main(String[] args) {
+		System.out.println("testando condicionais");
+		
+		int idade               = 16;
+		int quantidadeDePessoas = 3;
+		
+		if (idade >= 18) {
+			System.out.println("você tem mais de 18 anos!");
+			System.out.println("Seja bem vindo.");
+		}
+		else {
+			if(quantidadeDePessoas >= 2) {
+				System.out.println("Você não tem 18, porém esta acompanhado então pode entrar.");
+			} else {
+				System.out.println("Infelizmente você não pode entrar!");
+				
+			}	
+		}
+	}
+}
