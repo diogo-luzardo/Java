491fbf6ef68f6a9acad3cbd2861059ed53c2f21f
diogo-luzardo <54155950+diogo-luzardo@users.noreply.github.com>
2019-11-10 12:24:32 -0300

    Laços-Exercicios

---
+++ sintaxe-variaveis-e-fluxo/src/Variaveis/Multiplos3.java
@@ -0,0 +1,14 @@
+package Variaveis;
+
+public class Multiplos3 {
+
+	public static void main(String[] args) {
+		for(int multiplosDeTres = 1; multiplosDeTres <= 100; multiplosDeTres++) {
+			if(multiplosDeTres % 3 == 0) {
+				System.out.println(multiplosDeTres);
+			}
+		}
+
+	}
+
+}
