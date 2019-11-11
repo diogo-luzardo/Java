01cc81a2546c50e65e135131ee341ae803800799
diogo-luzardo <54155950+diogo-luzardo@users.noreply.github.com>
2019-11-11 19:00:47 -0300

    Construtores

--- bytebank-encapsulado/src/Conta.java
+++ bytebank-encapsulado/src/Conta.java
@@ -4,8 +4,14 @@ public class Conta {
 	private int numero;
 	private Cliente titular;
 	
+	public Conta(int agencia, int numero) {
+		this.agencia = agencia;
+		this.numero = numero;
+		System.out.println("Estou criando uma conta" + this.numero);
+	}
 	
-	public void deposita(double valor){
+	
+	public void deposita(double valor) {
 		this.saldo += valor;
 	}
 	
@@ -37,6 +43,9 @@ public class Conta {
 	}
 	
 	public void setNumero(int numero) {
+		if(numero <= 0) {
+			System.out.println("nao pode valor menor igual a 0");
+		}
 		this.numero = numero;
 	}
 	
@@ -45,7 +54,18 @@ public class Conta {
 	}
 	
 	public void setAgencia(int agencia) {
+		if(agencia <= 0) {
+			System.out.println("nao pode valor menor igual a 0");
+			return;
+		}
 		this.agencia = agencia;
 	}
 	
+	public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
+	
+	public Cliente getTitular() {
+		return this.titular;
+	}
 }
--- bytebank-encapsulado/src/TestaGetESet.java
+++ bytebank-encapsulado/src/TestaGetESet.java
@@ -2,9 +2,28 @@
 public class TestaGetESet {
 
 	public static void main(String[] args) {
-		Conta conta = new Conta();
-		conta.setNumero(1337);
+		Conta conta = new Conta(1337, 24226);
+		//conmta.numero = 1337;
+		//conta.setNumero(1337);
 		System.out.println(conta.getNumero());
+		
+		Cliente paulo = new Cliente();
+		//conta.titular = paulo;
+		
+		paulo.setNome("Paulo Silveira");
+		paulo.setCpf("222.222.222-22");
+		conta.setTitular(paulo);
+		
+		System.out.println(conta.getTitular().getNome());
+		
+		conta.getTitular().setProfissao("programador");
+		//agora em duas linhas
+		Cliente titularDaConta = conta.getTitular();
+		titularDaConta.setProfissao("Programador");
+		
+		System.out.println(titularDaConta);
+		System.out.println(conta.getTitular());
+		System.out.println(paulo);
 	}
 
 }
