5d9e6a62971d6ea681a222da143ec9122b0de175
diogo-luzardo <54155950+diogo-luzardo@users.noreply.github.com>
2019-11-17 17:08:20 -0300

    Interfaces

--- bytebank-herdado/src/Administrador.java
+++ bytebank-herdado/src/Administrador.java
@@ -1,5 +1,5 @@
 
-public class Administrador extends FuncionarioAutenticavel {
+public class Administrador extends Autenticavel {
 
 	private int senha;
 	
--- bytebank-herdado/src/Gerente.java
+++ bytebank-herdado/src/Gerente.java
@@ -1,5 +1,5 @@
 //Gerente eh um Funcionario ou herda da class Funcionario		
-public class Gerente extends FuncionarioAutenticavel {
+public class Gerente extends Funcionario {
 	
 	public double getBonificacao() {
 		System.out.println("Bonificacao do getente");
