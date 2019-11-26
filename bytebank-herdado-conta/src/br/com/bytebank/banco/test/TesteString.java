dfbd46bc51a89cee5d354fef945012777433a66b
diogo-luzardo <54155950+diogo-luzardo@users.noreply.github.com>
2019-11-26 18:27:16 -0300

    String metodos

--- bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteString.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteString.java
@@ -11,9 +11,37 @@ public class TesteString {
 		
 		String minisculo = nome.toLowerCase();
 		
+		char c = 'A';
+		char d = 'b';
+		String character = nome.replace(c, d);
+		
 		System.out.println(nome);
 		System.out.println(outro);
 		System.out.println(minisculo);
+		System.out.println(character);
+		
+		char e = nome.charAt(2);
+		System.out.println(e);
+		
+		int pos = nome.indexOf("ur");
+		System.out.println(pos);
+		
+		String sub = nome.substring(1);
+		System.out.println(sub);
+		
+		System.out.println(nome.length());
+		
+		for(int i = 0; i < nome.length(); i ++) {
+			System.out.println(nome.charAt(i));
+			
+		}
+		
+		String vazio = "";
+		System.out.println(vazio.isEmpty());
+		
+		String tirarEspaco = "               alura                   ";
+		String semEspaco = tirarEspaco.trim();
+		System.out.println(semEspaco);
 	}
 
 }
