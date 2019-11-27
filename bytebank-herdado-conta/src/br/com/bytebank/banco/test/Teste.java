db76e75008c2ee25234b77db888385f7703dd65b
diogo-luzardo <54155950+diogo-luzardo@users.noreply.github.com>
2019-11-27 19:35:57 -0300

    Iniciando aula de Array

--- bytebank-herdado-conta/src/br/com/bytebank/banco/test/Teste.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/Teste.java
@@ -1,41 +1,9 @@
 package br.com.bytebank.banco.test;
 
-import br.com.bytebank.banco.modelo.Cliente;
-import br.com.bytebank.banco.modelo.Conta;
-import br.com.bytebank.banco.modelo.ContaCorrente;
-import br.com.bytebank.banco.modelo.ContaPoupanca;
-
 public class Teste {
 
 	public static void main(String[] args) {
-		
-		System.out.println("x");
-		System.out.println(3);
-		System.out.println(false);
-		
-		ContaCorrente cc = new ContaCorrente(22, 33);
-		ContaPoupanca cp = new ContaPoupanca(33, 22);
-		Cliente cliente = new Cliente();
-		
-		System.out.println(cc);
-		System.out.println(cp);
-		
-		println(cc);
-	}
-	
-	static void println() {
-
-	}
-
-	static void println(int a) {
-
-	}
-
-	static void println(boolean valor) {
-
-	}
-	
-	static void println(Conta conta) {
+		// TODO Auto-generated method stub
 
 	}
 
