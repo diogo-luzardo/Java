b454457eef686b54618f5215d966b88f78ebbabf
diogo-luzardo <54155950+diogo-luzardo@users.noreply.github.com>
2020-01-05 10:33:45 -0300

    Servlet CRUD

--- gerenciador/src/br/com/alura/gerenciador/servelet/Banco.java
+++ gerenciador/src/br/com/alura/gerenciador/servelet/Banco.java
@@ -44,5 +44,14 @@ public class Banco {
 		}
 		
 	}
+
+	public Empresa buscaEmpresaPelaId(Integer id) {
+		for (Empresa empresa : lista) {
+			if(empresa.getId() == id) {
+				return empresa;
+			}
+		}
+		return null;
+	}
 	
 }
