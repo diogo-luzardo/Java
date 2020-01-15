c04b0784e44f41e72ad6fc59996b7b1de289e31e
diogo-luzardo <54155950+diogo-luzardo@users.noreply.github.com>
2020-01-15 17:44:59 -0300

    JPA

---
+++ Java Alura/financas/src/br/com/caelum/financas/teste/TesteConta.java
@@ -0,0 +1,26 @@
+package br.com.caelum.financas.teste;
+
+import javax.persistence.EntityManager;
+
+import br.com.caelum.financas.modelo.Conta;
+import br.com.caelum.financas.util.JPAUtil;
+
+public class TesteConta {
+	
+	public static void main(String[] args) {
+		Conta conta = new Conta();
+		conta.setTitular("Diogo");
+		conta.setBanco("Inter");
+		conta.setAgencia("123");
+		conta.setNumero("456");
+		
+		EntityManager em = new JPAUtil().getEntityManager();
+
+		em.getTransaction().begin();
+		em.persist(conta);
+		em.getTransaction().commit();
+		
+		em.close();
+	
+	}
+}
