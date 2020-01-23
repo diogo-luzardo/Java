d8bb4c8cfd6f92fca3bd16f683b8a9e5a939945f
diogo-luzardo <54155950+diogo-luzardo@users.noreply.github.com>
2020-01-23 18:27:33 -0300

    WIP

--- Java Alura/financas/src/br/com/caelum/financas/teste/TesteFuncoesJPQL.java
+++ Java Alura/financas/src/br/com/caelum/financas/teste/TesteFuncoesJPQL.java
@@ -1,10 +1,11 @@
 package br.com.caelum.financas.teste;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import javax.persistence.EntityManager;
-import javax.persistence.Query;
 
+import br.com.caelum.financas.dao.MovimentacaoDao;
 import br.com.caelum.financas.modelo.Conta;
 import br.com.caelum.financas.modelo.TipoMovimentacao;
 import br.com.caelum.financas.util.JPAUtil;
@@ -19,14 +20,12 @@ public class TesteFuncoesJPQL {
 		Conta conta = new Conta();
 		conta.setId(2);
 		
-		String jpql = "select sum(m.valor) from Movimentacao m where m.conta = :pConta" + 
-		" and m.tipo = :pTipo" +
-		" order by m.valor desc";
-		Query query = em.createQuery(jpql);
-		query.setParameter("pConta", conta);
-		query.setParameter("pTipo", TipoMovimentacao.SAIDA);
 		
-		BigDecimal soma = (BigDecimal)query.getSingleResult();
+		
+		MovimentacaoDao dao = new MovimentacaoDao(em);
+		List<Double> medias = dao.getMediasPorDiaETipo(TipoMovimentacao.SAIDA, conta);
+		
+		BigDecimal soma = (BigDecimal)dao.getSingleResult();
 		System.out.println("A soma é: " + soma);
 		
 		em.getTransaction().commit();
